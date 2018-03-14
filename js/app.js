e8ac661d8b684ac61b8591a0d61e82e4af1aa507
Angela Shalenko <ashalenko@genscape.com>
2018-03-14 09:55:58 -0400

    updating and cleaning up comments

--- js/app.js
+++ js/app.js
@@ -9,16 +9,6 @@ $("button").click(function(){
 //toggles bio content show/hide when button is clicked.
 
 
-// $('a').click(function(){
-//     $(this).addClass('visited');
-// });
-
-
-// $("a").click(function(){
-//     $("a.coral").removeClass(".coral")
-//     $(this).addClass(".coral");
-// });
-
 //I tried several ways to get my nav anchor elements to stay a coral color when on that page.  Below is my final answer. :) 
 
 $(function(){
